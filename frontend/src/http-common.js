45926e71de4d7d8af9ea6b5ead02a55d4677b8b4
vinceny <vinceny@live.fr>
2021-07-12 18:22:58 +0300

    ready for prod ?

---
+++ frontend/src/http-common.js
@@ -0,0 +1,17 @@
+
+// axios permit to pass and get informations from to frontend and from to backend 
+import axios from "axios";
+
+let user = JSON.parse(localStorage.getItem('user'));
+
+const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8080/api';
+
+export default axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Authorization': 'Bearer ' + user.accessToken,
+    "Content-type": "application/json",
+    "Content-Type": "multipart/form-data"
+  }
+});
+
