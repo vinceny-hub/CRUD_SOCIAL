5c3db4bd2f03d7c9d3e6577667387c6e188bbf13
vinceny <vinceny@live.fr>
2021-07-12 21:34:23 +0300

    ready for prod ?

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "administrator",
-  PASSWORD: "admin",
-  DB: "p7db",
+  HOST: "us-cdbr-east-04.cleardb.com",
+  USER: "bc01683a59f1ae",
+  PASSWORD: "39e7c0e4",
+  DB: "heroku_3b2b8dfa8d264d5",
   dialect: "mysql",
   pool: {
     max: 5,
