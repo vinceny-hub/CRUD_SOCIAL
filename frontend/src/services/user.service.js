b72dced81992c924293ae7aa7d67c5246d1af363
vinceny <vinceny@live.fr>
2021-07-13 16:35:28 +0300

    ready for prod ?

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 // const API_URL = 'http://localhost:8080/api/test/';
-const API_URL = process.env.NODE_ENV === 'production' ? '/api/test' : 'http://localhost:3000/api';
+const API_URL = process.env.NODE_ENV === 'production' ? '/api/test/' : 'http://localhost:3000/api';
 
 class UserService {
   getPublicContent() {
