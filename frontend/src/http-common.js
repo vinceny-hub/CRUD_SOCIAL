a6430c5474b962db4f739763b609c7fa2ffb71fd
vinceny <vinceny@live.fr>
2021-07-14 00:02:17 +0300

    ready for prod ?

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -3,20 +3,20 @@
 import axios from "axios";
 
 let user = JSON.parse(localStorage.getItem('user'));
-let accessToken;
+// let accessToken;
 
-if (process.env.NODE_ENV === 'production'){
-    accessToken = user.accessToken;
-} else {
-    accessToken = user ? user.accessToken : ''
-}
+// if (process.env.NODE_ENV === 'production'){
+//     accessToken = user.accessToken;
+// } else {
+//     accessToken = user ? user.accessToken : ''
+// }
 
 const API_URL = process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:3000/api/';
 
 export default axios.create({
   baseURL: API_URL,
   headers: {
-    'Authorization': 'Bearer ' + accessToken ,
+    'Authorization': 'Bearer ' + user.accessToken ,
     "Content-type": "application/json",
     "Content-Type": "multipart/form-data"
   }
