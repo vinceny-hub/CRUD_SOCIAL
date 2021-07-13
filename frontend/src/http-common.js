05eaea084a2e407c0dca36ef64be6218662dd4af
vinceny <vinceny@live.fr>
2021-07-13 16:21:44 +0300

    ready for prod ?

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 let user = JSON.parse(localStorage.getItem('user'));
 
-const API_URL = process.env.NODE_ENV === 'production'
-// : 'http://localhost:8080/api';
+const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api';
+console.log(process.env.NODE_ENV)
 
 export default axios.create({
   baseURL: API_URL,
