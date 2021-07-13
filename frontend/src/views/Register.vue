28aa37213948b361f621093d72a10539ef996314
vinceny <vinceny@live.fr>
2021-07-13 17:44:03 +0300

    ready for prod ?

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -3,7 +3,7 @@
     <div class="card card-container">
       <img
         id="profile-img"
-        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+        src="../img/titi0.webp"
         class="profile-img-card"
       />
       <form name="form" @submit.prevent="handleRegister">
