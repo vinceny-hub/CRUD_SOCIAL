317029320b9937a1dd51da530dcd17d9a0405824
vinceny <vinceny@live.fr>
2021-07-14 00:15:18 +0300

    ready for prod ?

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,35 +1,16 @@
 
 // axios permit to pass and get informations from to frontend and from to backend 
-// import axios from "axios";
-
-// let user = JSON.parse(localStorage.getItem('user'));
-// // let accessToken;
-
-// // if (process.env.NODE_ENV === 'production'){
-// //     accessToken = user.accessToken;
-// // } else {
-// //     accessToken = user ? user.accessToken : ''
-// // }
-
-// const API_URL = process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:3000/api/';
-
-// export default axios.create({
-//   baseURL: API_URL,
-//   headers: {
-//     'Authorization': 'Bearer ' + user.accessToken ,
-//     "Content-type": "application/json",
-//     "Content-Type": "multipart/form-data"
-//   }
-// });
-
-
 import axios from "axios";
+
 let user = JSON.parse(localStorage.getItem('user'));
-const API_URL = process.env.NODE_ENV 
+const accessToken = user ? user.accessToken : ' '
+
+const API_URL = process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:3000/api/';
+
 export default axios.create({
   baseURL: API_URL,
   headers: {
-    'Authorization': 'Bearer ' + user.accessToken,
+    'Authorization': 'Bearer ' + accessToken,
     "Content-type": "application/json",
     "Content-Type": "multipart/form-data"
   }
