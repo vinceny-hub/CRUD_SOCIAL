67771d14020987f90a59665a2139e63f742accc3
vinceny <vinceny@live.fr>
2021-08-14 14:21:24 +0300

    make it better

--- frontend1/src/models/user.js
+++
@@ -1,9 +0,0 @@
-export default class User {
-    constructor(username, email, password, userId) {
-      this.username = username;
-      this.email = email;
-      this.password = password;
-      this.userId = userId
-    }
-  }
-  
